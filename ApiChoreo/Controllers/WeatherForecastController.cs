5cfca3b888086b3ad36d6c218394b226b804dff2
christian-777 <christian.c.g.f777@hotmail.com>
2023-10-05 16:55:35 -0300

    dockerfile

--- ApiChoreo/Controllers/WeatherForecastController.cs
+++ ApiChoreo/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace ApiChoreo.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("get",Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
